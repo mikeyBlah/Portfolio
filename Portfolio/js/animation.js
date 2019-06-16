b2e98e30d9b1153a8b9d78d7229af751b0728c5e
Michael Redburn <redburnmichael@gmail.com>
2019-06-16 18:55:05 +1000

    Add files via upload

---
+++ Portfolio/js/animation.js
@@ -0,0 +1,45 @@
+window.onload = function(){
+
+    let scrollTop;
+    let scrollHeight = window.outerHeight;
+    let getScroll = setInterval(function(){
+    scrollTop = window.pageYOffset;
+    checkScroll();
+}, 500);
+ 
+
+function animateValue(skill, start, end, duration, bar, classer) {
+    let range = end - start;
+    let current = start;
+    let increment = end > start? 1 : -1;
+    let stepTime = Math.abs(Math.floor(duration / end));
+    let htmlCounter = document.querySelector('#htmlCounter');
+    let htmlBar = document.querySelector('#htmlBar');
+    let cssCounter = document.querySelector('#cssCounter');
+    let cssBar = document.querySelector('#cssBar');
+    let jsCounter = document.querySelector('#jsCounter');
+    let jsBar = document.querySelector('#jsBar');
+    let timer = setInterval(function() {
+        current += increment;
+        bar.classList.add(classer);
+        bar.classList.remove("hide");
+        skill.innerHTML = current + ".0";
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+    function checkScroll(){
+
+        if(scrollTop >= scrollHeight/4 * 2){   
+        animateValue(htmlCounter, 0, 8, 1000, htmlBar, "pieOne");
+        animateValue(cssCounter, 0, 8, 1000, cssBar, "pieTwo");
+        animateValue(jsCounter, 0, 5, 1000, jsBar, "pieThree");
+        clearInterval(getScroll);
+        console.log(scrollHeight /4 * 2)
+
+        }
+    }
+
+}
