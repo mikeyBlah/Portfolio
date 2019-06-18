c360d146b46ec8dcbebb6b18dd960a5b79e741e8
Michael Redburn <redburnmichael@gmail.com>
2019-06-18 20:46:57 +1000

    Menu and character changes

--- js/openSideNav.js
+++ js/openSideNav.js
@@ -3,6 +3,7 @@ window.addEventListener("load", function() {
 const menuTab = document.querySelector('#menuTab');
 const sideMenu = document.querySelector('#sideMenu');
 const overlay = document.querySelector('#fixedOverlay');
+const spanLine = document.querySelectorAll('.menuLine');
 let visible = false;
 
     menuTab.addEventListener("click", function(){
@@ -11,12 +12,20 @@ let visible = false;
         sideMenu.classList.remove('hideMenu');
         overlay.classList.add('showOverlay');
         overlay.classList.remove('hideOverlay');
+        for (i = 0; i < spanLine.length; i++){
+            spanLine[i].classList.add('closeLine');
+            spanLine[i].classList.remove('hamLine');
+        }
         visible = true;
         } else{
         sideMenu.classList.add('hideMenu');
         sideMenu.classList.remove('showMenu');
         overlay.classList.add('hideOverlay');
         overlay.classList.remove('showOverlay');
+        for (i = 0; i < spanLine.length; i++){
+            spanLine[i].classList.add('hamLine');
+            spanLine[i].classList.remove('closeLine');
+        }
         visible = false;
         }
     });
